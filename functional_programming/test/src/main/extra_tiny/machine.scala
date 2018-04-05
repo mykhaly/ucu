d5136f44e6eb96a53777f1117315c731a6da1e0d
Yurii Mykhalchuk <yurkamykhalchuk@gmail.com>
2018-04-05 21:38:57 +0300

    Tiny language v2.0

--- functional_programming/test/src/main/extra_tiny/machine.scala
+++ functional_programming/test/src/main/extra_tiny/machine.scala
@@ -1,16 +1,27 @@
 final class Machine {
-  def run(expr: Expr): Expr = {
+  def run(expr: Expr, env: Map[String, Int]): Expr = {
     println(expr)
 
     if (expr.isReducible) {
-      run(reductionStep(expr))
+      run(reductionStep(expr, env), env)
     }
     else {
       expr
     }
   }
 
-  def reductionStep(expr: Expr): Expr = {
-    expr.reduce
+  def reductionStep(expr: Expr, env: Map[String, Int]): Expr = {
+    expr match {
+      case Number(n) => Number(n)
+      case Mult(l, r) =>
+        if (l.isReducible) Mult(reductionStep(l, env), r)
+        else if (r.isReducible) Mult(l, reductionStep(r, env))
+        else Number(expr.evaluate)
+      case Sum(l, r) =>
+        if (l.isReducible) Sum(reductionStep(l, env), r)
+        else if (r.isReducible) Sum(l, reductionStep(r, env))
+        else Number(expr.evaluate)
+      case Var(x) => Number(env(x))
+    }
   }
 }
--- functional_programming/test/src/main/extra_tiny/main.scala
+++ functional_programming/test/src/main/extra_tiny/main.scala
@@ -2,8 +2,10 @@ object main {
   def main(args: Array[String]): Unit = {
 //    new Machine().run(Mult(Mult(Number(3), Number(2)), Mult(Number(7), Number(6))))
 //    new Machine().run(Mult(Number(2), Sum(Number(3), Number(6))))
-    new Machine().run(Sum(Mult(Number(2), Number(3)), Number(4)))
+//    val expression = Sum(Mult(Var("x"), Number(3)), Var("y"))
+    val expression = Mult(Sum(Var("x"), Number(2)), Sum(Number(4), Var("y")))
+    val environment = Map("x" -> 1, "y" -> 3)
+    new Machine().run(expression, environment)
   }
-
 }
 
